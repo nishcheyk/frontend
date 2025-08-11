1b4bd3b640dab0ea2d4ef260ddb6f95ebfc995c1
nishcheyk <nishcheycapture2014@gmail.com>
2025-08-11 09:42:11 +0530

    updates

---
+++ frontend/src/lottie.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Lottie = () => {
+  return <DotLottieReact src="./Recruitment.json" loop autoplay />;
+};
+
+export default Lottie;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import App from "./App";
+import Lottie from "./lottie";
 import Loader from "./loader";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <Loader></Loader>
+    <Lottie></Lottie>
   </Provider>
 );
